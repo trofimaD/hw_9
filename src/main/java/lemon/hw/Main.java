97d8fcd4324dc9155df75bffbd5de929b1aa74aa
trofimaD <trofima99@gmail.com>
2023-03-24 21:52:57 +0200

    added First task

--- src/main/java/lemon/hw/Main.java
+++ src/main/java/lemon/hw/Main.java
@@ -5,56 +5,43 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        //System.out.println(Arrays.toString(naturalNumberIterative(7)));
+        naturalNumberIterative(10);
+        naturalNumberRecursion(10);
 
     }
 
 
+//    // Дано натуральне число n. Вивести всі числа від 1 до n  ИТЕРАТИВНО, массив
+//    public  static int[] naturalNumberIterativeA (int n){
+//        int result[] = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            result[i] = i+1;
+//        }
+//        return result;
+//    }
 
-    // Дано натуральне число n. Вивести всі числа від 1 до n
-    public  static int[] naturalNumberIterative (int n){
-        int result[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = i+1;
+    // 1.Дано натуральне число n. Вивести всі числа від 1 до n  ИТЕРАТИВНО
+    public static void naturalNumberIterative(int n) {
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 
-            System.out.println(Arrays.toString(result));
 
-
-
+    // 1.Дано натуральне число n. Вивести всі числа від 1 до n  Рекурсия
+    public static void naturalNumberRecursion(int n) {
+        if (n > 0) {
+            naturalNumberRecursion(n - 1);
+            System.out.print(n + " ");
         }
 
-
-
-
-
-
-
-
-        return result;
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//    public static int sum(int k) {
-//        if (k > 0) {
-//            return k + sum(k - 1);
-//        } else {
-//            return 0;
-//        }
-//    }
 
 
 }
